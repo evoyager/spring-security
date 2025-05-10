912be1e6b6b3100f27a0fb6d2a6adf507ad9e46f
Ievgen Gusar <evgeniygusar@gmail.com>
2025-05-10 18:33:56 +0300

    Initial commit

---
+++ articles-client/src/main/java/com/epam/articles_client/ArticlesClientApplication.java
@@ -0,0 +1,13 @@
+package com.epam.articles_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesClientApplication.class, args);
+	}
+
+}
---
+++ articles-client/src/test/java/com/epam/articles_client/ArticlesClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.articles_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
